f1eeedef72122cbfd92f15dc04c46affb0380e63
robycigar <rabihutomo11@gmail.com>
2021-12-21 16:38:04 +0700

    menambahkan landing page

--- praktikum08-if4/index.php
+++ praktikum08-if4/index.php
@@ -1,34 +1,36 @@
-<?php 
+<?php
 include('config/db_conn.php');
 ?>
 
-<!doctype html> 
-<html> 
-<head> 
-  <meta charset="utf-8"> 
+<!doctype html>
+<html>
+
+<head>
+  <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <link rel="stylesheet" href="assets/css/bootstrap.css"> 
-  <title>Admin PWL</title> 
-</head> 
-<body> 
+  <link rel="stylesheet" href="assets/css/bootstrap.css">
+  <title>Admin PWL</title>
+</head>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="?menu=admin">Admin</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="?menu=artikel">Artikel</a>
-      </li>
-      <!-- <li class="nav-item dropdown">
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="?menu=admin">Admin</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="?menu=artikel">Artikel</a>
+        </li>
+        <!-- <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Siswa
         </a>
@@ -40,33 +42,34 @@ include('config/db_conn.php');
           <a class="dropdown-item" href="#">Alumni</a>
         </div>
       </li>   -->
-    </ul>
-    <span class="navbar-text mr-3">
-      <?php
-          if (isset($_SESSION['name'])) {
-          echo "Halo admin ".$_SESSION['name'];
-          }
-      ?>
-    </span>
+      </ul>
+      <span class="navbar-text mr-3">
+        <?php
+        if (isset($_SESSION['name'])) {
+          echo "Halo admin " . $_SESSION['name'];
+        }
+        ?>
+      </span>
 
-    <a href="config/logout.php" class="btn btn-outline-success mr-2">Logout</a>
-  </div>
-</nav>
-<br/>
-<br/>
+      <a href="config/logout.php" class="btn btn-outline-success mr-2">Logout</a>
+    </div>
+  </nav>
+  <br />
+  <br />
 
-<div class="module_content">
   <div class="module_content">
-    <div align="center">
-      <?php include "config/content.php"; ?>
+    <div class="module_content">
+      <div align="center">
+        <?php include "config/content.php"; ?>
+      </div>
     </div>
   </div>
-</div>
-     
-<hr>
- 
-<script src="assets/js/jquery.js"></script> 
-<script src="assets/js/popper.js"></script> 
-<script src="assets/js/bootstrap.js"></script>
-</body> 
+
+  <hr>
+
+  <script src="assets/js/jquery.js"></script>
+  <script src="assets/js/popper.js"></script>
+  <script src="assets/js/bootstrap.js"></script>
+</body>
+
 </html>
--- praktikum08-if4/modul/home/home.php
+++ praktikum08-if4/modul/home/home.php
@@ -1,3 +1,37 @@
+<?php
+include "../../config/db_conn.php";
 
-        <h1>Selamat Datang !</h1>
-        <p class="lead">Ini adalah halaman administrator<br> Sekolah milik Universitas AMIKOM Yogykarta</p>
+$sql = "SELECT * FROM artikel LIMIT 3";
+
+$result = mysqli_query($conn, $sql);
+
+// print_r($result);
+// die("here");
+?>
+
+<div class="container my-5" style="text-align: left;">
+        <h3>Anyeonhaseyo, Mas!</h3>
+        <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum sapiente aliquid nam perspiciatis, iste voluptatem provident explicabo nisi facilis architecto inventore ad illo dignissimos quisquam nulla eaque vero in tempore.
+        </p>
+        <button class="btn btn-primary">Learn more</button>
+</div>
+
+<div class="container my-5">
+        <div class="d-flex resposive justify-content-center">
+                <?php
+                while ($row = mysqli_fetch_array($result)) {
+                ?>
+                        <div style="width: 30%" class="card text-left p-4 mx-3">
+                                <img src="<?php echo $row['gambar']; ?>" alt="" class="card-img-top">
+                                <div class="card-text">
+                                        <h2><?php echo $row['judul']; ?></h2>
+                                        <p><?php echo $row['isi']; ?></p>
+                                </div>
+                                <button class="btn btn-secondary">View Details</button>
+                        </div>
+                <?php
+                }
+                ?>
+        </div>
+</div>
