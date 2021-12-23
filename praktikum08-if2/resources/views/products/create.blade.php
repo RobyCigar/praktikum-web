c66e29a308fafc2dbd89e766cde4fa0d8dd43753
robycigar <rabihutomo11@gmail.com>
2021-12-23 21:23:05 +0700

    add laravel crud

---
+++ praktikum08-if2/resources/views/products/create.blade.php
@@ -0,0 +1,38 @@
+@extends('products.layout')
+
+@section('content')
+
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <h2>Create Product</h2>
+    </div>
+</div>
+
+<form action="{{route('products.store')}}" method="post">
+    @csrf
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Name:</strong>
+                <input type="text" name="name" class="form-control" placeholder="Name">
+            </div>
+        </div> 
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Detail:</strong>
+                <textarea class="form-control" style="height:150px" name="detail" placeholder="Detail"></textarea>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Price:</strong>
+                <textarea class="form-control" style="height:150px" name="detail" placeholder="Detail"></textarea>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+</form>
+
+@endsection
---
+++ praktikum08-if2/resources/views/products/index.blade.php
@@ -0,0 +1,45 @@
+@extends('products.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <h1>Products</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Price</th>
+                        <th>&nbsp;</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($products as $product)
+                        <tr>
+                            <td>{{ $product->id }}</td>
+                            <td>{{ $product->name }}</td>
+                            <td>{{ $product->description }}</td>
+                            <td>{{ $product->price }}</td>
+                            <td>
+                                <a href="{{ route('products.show', $product->id) }}" class="btn btn-info">Show</a>
+                                <a href="{{ route('products.edit', $product->id) }}" class="btn btn-warning">Edit</a>
+                                <form action="{{ route('products.destroy', $product->id) }}" method="POST" class="d-inline">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    {!! $products->links() !!}
+@endsection
